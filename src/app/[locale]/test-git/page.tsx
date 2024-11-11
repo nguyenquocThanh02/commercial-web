2973ac7244629651c7caf40b4b9b6cbb9e10395f
quocThanh <2002nguyenquocthanh@gmail.com>
2024-11-11 20:42:44 +0700

    feat: list

--- src/app/[locale]/test-git/page.tsx
+++ src/app/[locale]/test-git/page.tsx
@@ -3,9 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      commit 1: tạo nhánh list
-      <div>commit 2: tạo nhánh details</div>
-      <div>commit 3: tạo nhánh update</div>
+      test lan 2<div>commit 1: nhanh list</div>
     </div>
   );
 };
